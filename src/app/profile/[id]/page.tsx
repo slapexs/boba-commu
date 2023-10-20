09fea5f06b4b818bb3671aedd28689c68db8f15e
onze <slapexs@gmail.com>
2023-10-20 18:33:09 +0700

    :lipstick: Move user profile to component

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,8 @@
 import React from "react"
-import Image from "next/image"
-import StatProfile from "@/components/StatProfile"
 import avatar from "../../../../public/favicon.png"
 
 import CardPost from "@/components/CardPost"
+import UserProfileHeader from "@/components/UserProfileHeader"
 
 type Props = {}
 
@@ -11,24 +10,7 @@ export default function Profile({}: Props) {
 	return (
 		<>
 			<main className="w-full">
-				<div className="flex gap-5">
-					<div>
-						<Image
-							alt="avatar"
-							src={avatar}
-							width={128}
-							height={128}
-							className="mask mask-squircle bg-primary"
-						/>
-					</div>
-					<div className="gap-3 flex flex-col">
-						<h1 className="text-2xl mt-4 font-semibold">Firstname lastname</h1>
-						<StatProfile />
-						<button type="button" className="btn  w-fit">
-							แก้ไขโปรไฟล์
-						</button>
-					</div>
-				</div>
+				<UserProfileHeader avatar={avatar} />
 				<div className="divider"></div>
 
 				{/* My post */}
---
+++ src/components/UserProfileHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Image, { StaticImageData } from "next/image"
+import StatProfile from "./StatProfile"
+type Props = {
+	avatar: StaticImageData
+}
+
+export default function UserProfileHeader({ avatar }: Props) {
+	return (
+		<div className="flex gap-5">
+			<div>
+				<Image
+					alt="avatar"
+					src={avatar}
+					width={128}
+					height={128}
+					className="mask mask-squircle bg-primary"
+				/>
+			</div>
+			<div className="gap-3 flex flex-col">
+				<h1 className="text-2xl mt-4 font-semibold">Firstname lastname</h1>
+				<StatProfile />
+				<button type="button" className="btn  w-fit">
+					แก้ไขโปรไฟล์
+				</button>
+			</div>
+		</div>
+	)
+}
