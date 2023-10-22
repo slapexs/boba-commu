4884864f562532131ddca834b9bbb6c25dc30b78
onze <slapexs@gmail.com>
2023-10-22 19:31:00 +0700

    :sparkles: Add state show/hide menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+"use client"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
 
 const Navbar = () => {
+	const path = usePathname()
+	const [showMenu, setShowMenu] = useState<boolean>(true)
+
+	useEffect(() => {
+		if (path == "/login" || path == "/register") {
+			setShowMenu(false)
+		} else {
+			setShowMenu(true)
+		}
+	}, [path])
+
 	return (
 		<>
 			<nav className="navbar bg-base-100 w-screen flex justify-center drop-shadow-sm fixed top-0 z-10">
@@ -12,24 +25,26 @@ const Navbar = () => {
 						</Link>
 					</div>
 					<div className="flex-none">
-						<ul className="menu menu-horizontal px-1 gap-x-5">
-							<li>
-								<Link href="/search">ค้นหา</Link>
-							</li>
-							<li>
-								<details>
-									<summary>Profile</summary>
-									<ul className="p-2 bg-base-100">
-										<li>
-											<Link href="/profile/1">โปรไฟล์</Link>
-										</li>
-										<li>
-											<a>ออกจากระบบ</a>
-										</li>
-									</ul>
-								</details>
-							</li>
-						</ul>
+						{showMenu && (
+							<ul className="menu menu-horizontal px-1 gap-x-5">
+								<li>
+									<Link href="/search">ค้นหา</Link>
+								</li>
+								<li>
+									<details>
+										<summary>Profile</summary>
+										<ul className="p-2 bg-base-100">
+											<li>
+												<Link href="/profile/1">โปรไฟล์</Link>
+											</li>
+											<li>
+												<a>ออกจากระบบ</a>
+											</li>
+										</ul>
+									</details>
+								</li>
+							</ul>
+						)}
 					</div>
 				</main>
 			</nav>
